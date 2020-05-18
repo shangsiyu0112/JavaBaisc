3efb868fc6f958f21738b90a7166d14efe08db07
思雨 <1205063467@qq.com>
2020-05-19 01:23:40 +0800

    char[] arr1 = new char[]{'a','b','c','d','e','f','g','h' ,' ',' '};         System.out.println(arr1); //地址值？

---
+++ src/basic/ArrayPrintTest.java
@@ -0,0 +1,14 @@
+package basic;
+
+/**
+ * @athor 尚思雨
+ * @date 2020-05-19-1:11
+ */
+public class ArrayPrintTest {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        System.out.println(arr);//地址值?
+        char[] arr1 = new char[]{'a','b','c','d','e','f','g','h' ,' ',' '};
+        System.out.println(arr1); //地址值？
+    }
+}
