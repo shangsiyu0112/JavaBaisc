f3ad42fb92269a48297da2c9baaf7999e59f2243
思雨 <1205063467@qq.com>
2020-05-18 19:50:00 +0800

    99乘法表

---
+++ src/basic/Nineninetable.java
@@ -0,0 +1,30 @@
+package basic;
+
+/**
+ * @athor 尚思雨
+ * @date 2020-05-18-19:47
+ */
+/*
+嵌套循环的应用1：
+
+九九乘法表
+1 * 1 = 1
+2 * 1 = 2  2 * 2 = 4
+。。。
+9 * 1 = 9 。。。 9 * 9 = 81
+
+
+*/
+public class Nineninetable {
+    public static void main(String[] args) {
+        for(int i = 1;i <= 9;i++){
+
+            for(int j = 1;j <= i;j++){
+                System.out.print(i + " * " + j + " = " + (i * j) + "  ");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
