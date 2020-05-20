daf85150e8138771c65f171a741070df598ef5f2
思雨 <1205063467@qq.com>
2020-05-20 12:22:03 +0800

    测试异常

---
+++ src/oop/ThrowException.java
@@ -0,0 +1,46 @@
+package oop;
+
+/**
+ * @athor 尚思雨
+ * @date 2020-05-20-11:21
+ */
+public class ThrowException {
+    public static void main(String[] args) {
+        try {
+            Student s = new Student();
+            s.regist(-1001);
+            System.out.println(s);
+        } catch (Exception e) {
+//			e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
+
+
+class Student{
+
+    private int id;
+
+    public void regist(int id) throws Exception {
+        if(id > 0){
+            this.id = id;
+        }else{
+//			System.out.println("您输入的数据非法！");
+            //手动抛出异常对象
+//			throw new RuntimeException("您输入的数据非法！");
+//			throw new Exception("您输入的数据非法！");
+            throw new MyException("不能输入负数");
+            //错误的
+//			throw new String("不能输入负数");
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Student [id=" + id + "]";
+    }
+
+}
