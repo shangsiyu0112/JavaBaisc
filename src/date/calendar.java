75aaa901d4edfd51fe2768cde9f1994ad2f62c0f
思雨 <1205063467@qq.com>
2020-05-21 20:57:36 +0800

    日历类

---
+++ src/date/calendar.java
@@ -0,0 +1,15 @@
+package date;
+
+import java.util.Calendar;
+
+/**
+ * @athor 尚思雨
+ * @date 2020-05-21-16:46
+ */
+public class calendar {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.getClass());
+
+    }
+}
